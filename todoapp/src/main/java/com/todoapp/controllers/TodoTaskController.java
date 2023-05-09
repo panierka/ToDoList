47643691f0a34c5774447827fad92afaa8cc6995
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-05-09 19:08:01 +0200

    feat: logger; fix: tasks retrieval

--- todoapp/src/main/java/com/todoapp/controllers/TodoTaskController.java
+++ todoapp/src/main/java/com/todoapp/controllers/TodoTaskController.java
@@ -2,19 +2,23 @@ package com.todoapp.controllers;
 
 import com.todoapp.models.TodoTask;
 import com.todoapp.services.TodoTaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@Slf4j
 public class TodoTaskController {
     @Autowired
     private TodoTaskService service;
 
     @GetMapping(value = "/task/get-all")
     public List<TodoTask> getAllTasks(){
-        return service.getAllTasks();
+        var tasks = service.getAllTasks();
+        log.info("getting {} tasks", tasks.size());
+        return tasks;
     }
 }
